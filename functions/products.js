238d12b29a3939a51fd0f197d2936efc042d55fc
Avijit Karmaker <ajkacca@gmail.com>
2022-10-02 22:26:55 +0300

    add all files

--- functions/products.js
+++ functions/products.js
@@ -5,51 +5,58 @@ const airtable= new Airtable({apiKey:process.env.AIRTABLE_API_KEY})
 .base('app7Qq9maEDt4e96B')
 .table('products')
 
-
-exports.handler=async (event, context)=> {
-    
-    let { id }= event.queryStringParameters;
-    console.log(id);
-    if(id) {
-        try {
-            let product=await airtable.retrieve(id);
-            if(product.error) {
-                return {
-                    statusCode:200,
-                    body: `no product found with ${id}`
-                }                
-            }
-            return {
-                statusCode:200,
-                body: JSON.stringify(product)
-            }
-        } catch (error) {
-            return {
-                statusCode:500,
-                body: `no product found with ${id}`
-            }
-        }
-
+exports.handler=async(event,context)=> {
+    return {
+        statuCode:200,
+        body:"hello it is working"
     }
 
-    try {
-        let {records}= await airtable.list();
-        let products= records.map(item=> {
-            let {id, fields}= item;
-            let {Price, Category, Description, Company, Name, Image, Rating}= fields;
-    
-            return {
-                id, Price, Category, Description, Company, Name, Image, Rating 
-            }
-        })
-        return {
-            statusCode:200,
-            body: JSON.stringify(products)
-        }   
-    } catch (error) {
-        return {
-            statusCode: 404,
-            body:error
-        }
-    }
 }
+
+// exports.handler=async (event, context)=> {
+    
+//     let { id }= event.queryStringParameters;
+//     console.log(id);
+//     if(id) {
+//         try {
+//             let product=await airtable.retrieve(id);
+//             if(product.error) {
+//                 return {
+//                     statusCode:200,
+//                     body: `no product found with ${id}`
+//                 }                
+//             }
+//             return {
+//                 statusCode:200,
+//                 body: JSON.stringify(product)
+//             }
+//         } catch (error) {
+//             return {
+//                 statusCode:500,
+//                 body: `no product found with ${id}`
+//             }
+//         }
+
+//     }
+
+//     try {
+//         let {records}= await airtable.list();
+//         let products= records.map(item=> {
+//             let {id, fields}= item;
+//             let {Price, Category, Description, Company, Name, Image, Rating}= fields;
+    
+//             return {
+//                 id, Price, Category, Description, Company, Name, Image, Rating 
+//             }
+//         })
+//         return {
+//             statusCode:200,
+//             body: JSON.stringify(products)
+//         }   
+//     } catch (error) {
+//         return {
+//             statusCode: 404,
+//             body:error
+//         }
+//     }
+// }
